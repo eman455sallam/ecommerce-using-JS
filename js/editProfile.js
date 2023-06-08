60cc6d5f0e66ed36a56fce0401e7b307fcd8e1f7
eman ahmed <eman455sallam@gmail.com>
2023-06-08 16:38:40 +0300

    eman

--- js/editProfile.js
+++ js/editProfile.js
@@ -6,6 +6,7 @@ let userNameContainer=document.getElementById("userInfoName");
 let userEmailContainer=document.getElementById("userInfoEmail");
 let userForm=document.getElementById("userForm");
 let userImg=document.getElementById("userInfoImg");
+let imageUrl;
 
 userNameContainer.value=storedUserName;
 userEmailContainer.value=storedUserEmail;
@@ -18,11 +19,52 @@ function updateFunction(e){
     storedUserName=userNameContainer.value;
     storedUserEmail=userEmailContainer.value;
 
+
     localStorage.setItem("username" ,storedUserName);
     localStorage.setItem("email" ,storedUserEmail);
+    localStorage.setItem("userImgUrl",imageUrl);
 
     setTimeout(() =>{
         window.location.assign("index.html")
     },500);
 }
 
+
+userImg.addEventListener("change" ,uploadProImageFunction);
+
+//upload image
+function uploadProImageFunction(){
+    let file=this.files[0];  //this refers to files is default obj contains all files
+    
+    let types=["image/jpg" ,"image/png"];
+    if( types.indexOf(file.type ) == -1){
+        alert("this file not supported");
+        return;
+    }
+
+    if(file.size > 2*1024*1024*1024){
+        alert("this size not supported");
+        return;
+    }
+
+    getImageBase64(file);
+}
+
+
+//convert image to base64 so can be  uploaded
+
+function getImageBase64(file){
+    let reader=new FileReader();
+
+    reader.readAsDataURL(file);
+
+    reader.onload=function(){
+
+        imageUrl=reader.result;
+    };
+
+    reader.onerror=function(){
+
+        alert("error");
+    };
+}
--- js/profile.js
+++ js/profile.js
@@ -1,20 +1,28 @@
 
 let storedUserName=localStorage.getItem("username");
 let storedUserEmail=localStorage.getItem("email");
+let storedImgUrl=localStorage.getItem("userImgUrl");
 let allProducts=JSON.parse(localStorage.getItem("products"));
 let myProducts=allProducts.filter((e) => e.isMyProduct === "yes");
 
 let userNameContainer=document.getElementById("userName");
 let emailContainer=document.getElementById("userEmail");
+let imgContainer=document.getElementById("userImg").getAttribute('src');
 let productsNum=document.querySelector("#productsNum span");
 
 userNameContainer.innerHTML=storedUserName;
 emailContainer.innerHTML=storedUserEmail;
 
+console.log(storedImgUrl);
 if(myProducts.length != 0){
     productsNum.innerHTML=myProducts.length;
 }else{
     productsNum.innerHTML="new member";
+}
+
+if(storedImgUrl){
+    imgContainer.innerHTML=storedImgUrl;
+
 }
 
 
